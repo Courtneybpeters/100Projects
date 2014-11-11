7983e9e9d2b069b2fc9e1707261f959fca88e631
Courtney Peters <courtney@candldev.com>
2014-11-11 16:51:09 -0600

    Added support for consonant clusters of any length.

--- text/pig_latin_translator.py
+++ text/pig_latin_translator.py
@@ -4,32 +4,42 @@ import re
 def pig_latin_translator(phrase, vowel_method):
 
 	vowels = ['a', 'e', 'i', 'o', 'u']
+	translated = []
+	cluster = ''
 
 	#How will I find a way to capture what punctuation was removed and add it back after phrase is translated?
 	word_list = re.findall(r'\w+', phrase)
 	#DEBUGGING - print word_list
-	translated = []
-
+	
 	#Phrase loop - Translates each word separately
 	for word in word_list:
 
+		for x in range(len(word)):
+			if word[x] not in vowels:
+				cluster += (word[x])
+
+			elif word[x] in vowels:
+				translated.append(word[x:] + cluster + "ay")
+				cluster = ''
+				break
+
 		#Checks to see if word begins in a vowel.
-		if word[0] not in vowels:
-			if word[1] not in vowels:
-				translated.append(word[2:len(word)] + word[:2] + "ay")
-			else:	
-				translated.append(word[1:len(word)] + word[0] + "ay")
+		# if word[0] not in vowels:
+		# 	if word[1] not in vowels:
+		# 		translated.append(word[2:len(word)] + word[:2] + "ay")
+		# 	else:	
+		# 		translated.append(word[1:len(word)] + word[0] + "ay")
 
 		#Multiple methods for handling vowels that start with a vowel. 
-		if word[0] in vowels:
-			if vowel_method == 1:
-				translated.append(word + "way")
-			elif vowel_method == 2:
-				translated.append(word[1:len(word)] + word[0] + "way")
-			elif vowel_method == 3:
-				translated.append(word[1:len(word)] + word[0] + "i")
-			else:
-				print "Please select an option 1-3."
+		# if word[0] in vowels:
+		# 	if vowel_method == 1:
+		# 		translated.append(word + "way")
+		# 	elif vowel_method == 2:
+		# 		translated.append(word[1:len(word)] + word[0] + "way")
+		# 	elif vowel_method == 3:
+		# 		translated.append(word[1:len(word)] + word[0] + "i")
+		# 	else:
+		# 		print "Please select an option 1-3."
 
 	#Joins all the separated words back into a string
 	return_string = ' '.join(translated)
