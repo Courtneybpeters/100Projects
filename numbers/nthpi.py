beddc1f4fdd552eff403a464e52062b394db2c0b
Courtney Peters <courtneybpeters@gmail.com>
2014-11-17 14:26:43 -0600

    Changed code for readability and wrote a program to display pi at a certain decimal point.

---
+++ numbers/nthpi.py
@@ -0,0 +1,15 @@
+#Import math module
+import math
+
+#Get pi using math module.
+pi = math.pi
+
+#What number of decimal points they want.
+place = int(raw_input("What place would you like to evaluate pi to? "))
+
+#When i made the string first, it doesn't mess up.
+#Assuming at some point the format function messes up before formatting the string.
+place_string = '.%if' % place
+
+#Format allows you to format a number how you like.
+print format(pi, place_string)
